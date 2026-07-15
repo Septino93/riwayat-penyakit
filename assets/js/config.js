87c2a01f0df19245598de96047d740cf5d75e1fb
Septino93 <vsh5b2yfgj@privaterelay.appleid.com>
2026-07-15 13:39:35 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  API_URL: "PASTE_APPS_SCRIPT_WEB_APP_URL_HERE",
+  API_URL: "https://script.google.com/macros/s/AKfycbxYfyzp0Rdhsq9DQq3QVFFA_htXl1VDLTIHO59_eSyxgN3d-rj7im6g26vMraSLfvGh/exec",
   APP_NAME: "Kuisioner Riwayat Penyakit"
 };
